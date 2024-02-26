4610edb14e30f202e432c04adbfa437c9c1f09b1
ChavesVolodymyr <151963528+ChavesVolodymyr@users.noreply.github.com>
2024-02-26 20:32:45 +0100

    01

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function formatMessage(message, maxLength) {
-    if (message <= maxLength) {
+    if (message.lenght <= maxLength) {
         return message;   
     } else {
         return message.slice(0, maxLength) + '...';
