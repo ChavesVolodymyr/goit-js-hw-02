965ab756cee2a9a6065633d1812f68ff1df3367a
ChavesVolodymyr <151963528+ChavesVolodymyr@users.noreply.github.com>
2024-03-26 00:42:07 +0100

    2
    35

--- js/my-task-module6.js
+++ js/my-task-module6.js
@@ -18,3 +18,145 @@
 // console.log(pizzaPalace.order('Four meats'));
 // console.log(pizzaPalace.order('Big Mike'));
 // console.log(pizzaPalace.order('Viennese'));
+
+// class Car {
+//   #brand;
+//   constructor(param) {
+//     this.#brand = param.brand;
+//     this.model = param.model;
+//     this.price = param.price;
+//   }
+//   getPrice() {
+//     return this.price;
+//   }
+
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+//   getBrand() {
+//     return this.#brand;
+//   }
+
+//   changeBrand(newPrice) {
+//     this.#brand = newPrice;
+//   }
+// }
+
+// const car1 = new Car({
+//   brand: 'Audi',
+//   model: 'Q3',
+//   price: 36000,
+// });
+// console.log(car1.getPrice());
+
+// const car2 = new Car({
+//   brand: 'BMW',
+//   model: 'X5',
+//   price: 58900,
+// });
+// car2.changePrice(180250);
+// console.log(car2);
+
+// const car3 = new Car({
+//   brand: 'Nissan',
+//   model: 'Murano',
+//   price: 31700,
+// });
+// console.log(car3);
+
+// class Car {
+//   #price;
+//   static maxPrice = 50000;
+//   constructor(params) {
+//     this.#price = params.price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+// if (price > maxPrice) {
+//     // if (newPrice => Car.maxPrice) { //!! Чому не так?
+//     if (newPrice <= Car.maxPrice) {
+//       this.#price = newPrice;
+//     }
+//   }
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// class Car {
+//   static #maxPrice = 50000;
+
+//   constructor(params) {
+//     this.price = params.price;
+//   }
+//   static checkPrice(price) {
+//     if (price >= Car.#maxPrice) {
+//       return 'Error! Price exceeds the maximum';
+//     }
+//     return 'Success! Price is within acceptable limits';
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  static role = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser',
+  };
+
+  constructor(params) {
+    super(params.email);
+    this.access = params.access;
+    this.blacklistedEmails = [];
+  }
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  access: Admin.role.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.access); // "superuser"
+
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted('mango@mail.com')); // false
+console.log(mango.isBlacklisted('poly@mail.com')); // true
