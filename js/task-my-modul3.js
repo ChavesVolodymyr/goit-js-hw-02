f8e228f9c84477ed8526913cb97f545e43731632
ChavesVolodymyr <151963528+ChavesVolodymyr@users.noreply.github.com>
2024-03-28 16:54:10 +0100

    111

--- js/task-my-modul3.js
+++ js/task-my-modul3.js
@@ -102,16 +102,16 @@
 // console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
 // console.log(getCommonElements([1, 2, 3], [2, 4]));
 
-function calculateTax(amount, taxRate = 0.2) {
-  let totalTax = amount * taxRate;
-  return totalTax;
-}
-console.log(calculateTax(100, 0.1)); // 10
-console.log(calculateTax(200, 0.1)); // 20
-console.log(calculateTax(100, 0.2)); // 20
-console.log(calculateTax(200, 0.2)); // 40
-console.log(calculateTax(100, 0.3)); // 30
-console.log(calculateTax(200, 0.3)); // 60
-console.log(calculateTax(100)); // 20
-console.log(calculateTax(200)); // 40
+// function calculateTax(amount, taxRate = 0.2) {
+//   let totalTax = amount * taxRate;
+//   return totalTax;
+// }
+// console.log(calculateTax(100, 0.1)); // 10
+// console.log(calculateTax(200, 0.1)); // 20
+// console.log(calculateTax(100, 0.2)); // 20
+// console.log(calculateTax(200, 0.2)); // 40
+// console.log(calculateTax(100, 0.3)); // 30
+// console.log(calculateTax(200, 0.3)); // 60
+// console.log(calculateTax(100)); // 20
+// console.log(calculateTax(200)); // 40
 // ?????домашня робота №3
--- js/task-my-modul4.js
+++ js/task-my-modul4.js
@@ -1,16 +1,3 @@
-// const apartment = {
-//   descr: "Spacious apartment in the city center",
-//   rating: 4,
-//   price: 2153,
-// };
-// const values = ["Spacious apartment in the city center", 4, 2153];
-// const keys = Object.keys(apartment);
-// for (let key of keys) {
-  
-// }
-// console.log(key);
-
-
 // function countProps(object) {
 //   return Object.keys(object).length;
 // }
@@ -30,109 +17,6 @@
 // console.log(countTotalSalary({})); // Повертає 0
 // console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 })); // Повертає 330
 // console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 })); // Повертає 400
-
-
-// const apartment = {
-//   descr: "Spacious apartment in the city center",
-//   rating: 4,
-//   price: 2153,
-// };
-
-// const keys = Object.keys(apartment);
-// const values = [];
-
-// for (let key of keys) {
-//   values.push(apartment[key]);
-// }
-
-// console.log(keys); // ["descr", "rating", "price"]
-// console.log(values); // ["Spacious apartment in the city center", 4, 2153]
-
-
-// const colors = [
-//   { hex: "#f44336", rgb: "244,67,54" },
-//   { hex: "#2196f3", rgb: "33,150,243" },
-//   { hex: "#4caf50", rgb: "76,175,80" },
-//   { hex: "#ffeb3b", rgb: "255,235,59" },
-// ];
-
-// const hexColors = [];
-// const rgbColors = [];
-
-// for (let color of colors) {
-//   hexColors.push(color.hex);
-//   rgbColors.push(color.rgb);
-// }
-// console.log(hexColors);
-
-// console.log(rgbColors);
-
-// function getProductPrice(productName) {
-//   const products = [
-//     { name: "Radar", price: 1300, quantity: 4 },
-//     { name: "Scanner", price: 2700, quantity: 3 },
-//     { name: "Droid", price: 400, quantity: 7 },
-//     { name: "Grip", price: 1200, quantity: 9 },
-//     ];
-    
-//     for (const product of products) {
-//     if (product.name === productName) {
-//         return product.price;
-//     }
-// } return null;
-
-// }
-
-// console.log(getProductPrice("Radar")); // Повертає 1300
-// console.log(getProductPrice("Grip")); // Повертає 1200
-// console.log(getProductPrice("Scanner")); // Повертає 2700
-// console.log(getProductPrice("Droid")); // Повертає 400
-// console.log(getProductPrice("Engine")); // Повертає null
-
-// !!! it's HARD
-// function getAllPropValues(propName) {
-//   const products = [
-//     { name: "Radar", price: 1300, quantity: 4 },
-//     { name: "Scanner", price: 2700, quantity: 3 },
-//     { name: "Droid", price: 400, quantity: 7 },
-//     { name: "Grip", price: 1200, quantity: 9 },
-//     ];
-    
-//     const result = [];
-//     for (const product of products) {
-//         if (Object.keys(product).includes(propName)) {
-//             result.push(product[propName]);
-//         }
-//     } return result;
-// }
-
-
-// console.log(getAllPropValues("name")); // ["Radar", "Scanner", "Droid", "Grip"]
-// console.log(getAllPropValues("quantity")); // [4, 3, 7, 9]
-// console.log(getAllPropValues("price")); // [1300, 2700, 400, 1200]
-// console.log(getAllPropValues("category")); // []
-// !!!!!!
-
-// function calculateTotalPrice(productName) {
-//   const products = [
-//     { name: "Radar", price: 1300, quantity: 4 },
-//     { name: "Scanner", price: 2700, quantity: 3 },
-//     { name: "Droid", price: 400, quantity: 7 },
-//     { name: "Grip", price: 1200, quantity: 9 },
-//     ];
-//     for (const product of products) {
-//         if (product.name === productName) {
-//           return product.price * product.quantity
-//         }
-//     }  return `Product ${productName} not found!`
-// }
-
-// console.log(calculateTotalPrice("Blaster")); // "Product Blaster not found!"
-// console.log(calculateTotalPrice("Radar")); // 5200
-// console.log(calculateTotalPrice("Droid")); // 2800
-// console.log(calculateTotalPrice("Grip")); // 10800
-// console.log(calculateTotalPrice("Scanner")); // 8100
-
 
 // !!!!!!!!!!!!
 // Використовуючи синтаксис залишкових параметрів,
@@ -171,7 +55,6 @@
 // console.log(addOverNum(15, 32, 6, 13, 19, 8)); // Поверне 51
 // console.log(addOverNum(20, 74, 11, 62, 46, 12, 36)); // Поверне 218
 
-
 // function getExtremeScores(scores) {
 //     const best = Math.max(...scores);
 //     const worst = Math.min(...scores);
@@ -180,4 +63,111 @@
 
 // console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26])); // Поверне об'єкт { best: 93, worst: 17 }
 // console.log(getExtremeScores([19, 7, 4, 17, 81, 24])); // Поверне об'єкт { best: 81, worst: 4 }
+// //!! перебір об'єкта🤬
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
 
+// const keys = ['descr', 'rating', 'price'];
+// const values = [];
+// for (const keyIndex in keys) {
+//   const key = keys[keyIndex]; //!!!без цїєї змінної
+
+//   values.push(apartment[key]); //! А тут записати  values.push(apartment[keyIndex])
+// }
+// console.log(values);
+//!МАСИВ Об'єКТІВ
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+//? Розвязання видалене..
+// console.log(hexColors);
+
+// console.log(rgbColors);
+//! Пошук об'єкта за значенням властив
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//? Розвязання видалене..
+//     ]; }
+
+// console.log(getProductPrice("Radar")); // Повертає 1300
+// console.log(getProductPrice("Grip")); // Повертає 1200
+// console.log(getProductPrice("Scanner")); // Повертає 2700
+// console.log(getProductPrice("Droid")); // Повертає 400
+// console.log(getProductPrice("Engine")); // Повертає null
+
+// !!! it's HARD
+//!!Колекція значень властивості
+//function getAllPropValues(propName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+//   const keys = [];
+//   for (const product of products) {
+//     if (Object.keys(product).includes(propName)) {
+//       keys.push(product[propName]);
+//     }
+//   }
+//   return keys;
+// }
+
+// console.log(getAllPropValues('name')); // ["Radar", "Scanner", "Droid", "Grip"]
+// console.log(getAllPropValues('quantity')); // [4, 3, 7, 9]
+// console.log(getAllPropValues('price')); // [1300, 2700, 400, 1200]
+// console.log(getAllPropValues('category')); // []
+
+// ???? #2
+// function calculateTotalPrice(productName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+//   //? Розвязання видалене..
+// }
+
+// console.log(calculateTotalPrice('Blaster')); // "Product Blaster not found!"
+// console.log(calculateTotalPrice('Radar')); // 5200
+// console.log(calculateTotalPrice('Droid')); // 2800
+// console.log(calculateTotalPrice('Grip')); // 10800
+// console.log(calculateTotalPrice('Scanner')); // 8100
+// !!!!!!
+
+//!! МЕТОДИ ОБ'ЄКТА
+// const atTheOldToad = {
+//   potions: [
+//     { name: 'Speed potion', price: 460 },
+//     { name: 'Stone skin', price: 520 },
+//   ],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(potionName) {
+//     this.potions.push(potionName);
+//   },
+//   getTotalPrice() {
+//     let totalPrice = 0;
+//     for (let potion of this.potions) {
+//       totalPrice += potion.price;
+//     }
+//     return totalPrice;
+//   },
+// };
+// atTheOldToad.addPotion({ name: 'Invisibility', price: 620 });
+// atTheOldToad.addPotion({ name: 'Power potion', price: 270 });
+// console.log(atTheOldToad.getPotions());
+// console.log(atTheOldToad.getTotalPrice());
+//
